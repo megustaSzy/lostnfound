93b167a79f929506f6eb0a3dbfb34c81de56f667
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-28 13:24:31 +0700

    fix: perbaiki bug

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import { AxiosError } from "axios";
 import { api } from "@/lib/api";
-import { FormData, MessageType } from "@/types/profile";
+import { FormData as ProfileFormData, MessageType } from "@/types/profile";
 import { User } from "@/types/user";
 
 export function useProfile(
   user: User | null,
   refreshUser: () => Promise<void>
 ) {
-  const [formData, setFormData] = useState<FormData>({
+  const [formData, setFormData] = useState<ProfileFormData>({
     name: "",
     email: "",
     notelp: "",
     password: "",
+    image: null,
   });
 
   const [saving, setSaving] = useState(false);
@@ -25,14 +26,26 @@ export function useProfile(
         email: user.email,
         notelp: user.notelp || "",
         password: "",
+        image: null,
       });
     }
   }, [user]);
 
+  // input text
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+
+    if (name === "image" && files) {
+      setFormData((prev) => ({
+        ...prev,
+        image: files[0],
+      }));
+      return;
+    }
+
     setFormData((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value,
+      [name]: value,
     }));
   };
 
@@ -42,18 +55,26 @@ export function useProfile(
     try {
       setSaving(true);
 
-      const payload: Partial<FormData> = {};
+      const payload = new window.FormData();
 
-      if (formData.name !== user.name) payload.name = formData.name;
-      if (formData.email !== user.email) payload.email = formData.email;
-      if (formData.notelp !== user.notelp) payload.notelp = formData.notelp;
-      if (formData.password) payload.password = formData.password;
+      if (formData.name !== user.name) payload.append("name", formData.name);
+      if (formData.email !== user.email)
+        payload.append("email", formData.email);
+      if (formData.notelp !== user.notelp)
+        payload.append("notelp", formData.notelp || "");
+      if (formData.password) payload.append("password", formData.password);
+      if (formData.image) payload.append("image", formData.image);
 
       await api.patch(`/api/users/${user.id}`, payload);
-      await refreshUser();
 
+      await refreshUser();
       setMessage({ type: "success", text: "Profile berhasil diperbarui!" });
-      setFormData((prev) => ({ ...prev, password: "" }));
+
+      setFormData((prev) => ({
+        ...prev,
+        password: "",
+        image: null,
+      }));
     } catch (err) {
       const error = err as AxiosError<{ message: string }>;
       setMessage({
